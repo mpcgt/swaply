34812a57de74ac3d46ce5f1c320c85242ec99c5d
Max <max.pcgt@gmail.com>
2025-02-06 16:57:29 +0100

    📝 docs: Modifications to the Category Controller and creation of Twig pages for all categories - v.2.0.6-beta.2

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,11 +17,95 @@ final class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/category/browser', name: 'app_category')]
-    public function browser(): Response
+    #[Route('/category/browser', name: 'app_category_browser')]
+    public function browser(ProductsRepository $productsRepository): Response
     {
+        $products = $productsRepository->findBy(['category' => 1]);
+    
         return $this->render('category/browser.html.twig', [
             'controller_name' => 'CategoryController',
+            'products' => $products,
         ]);
     }
+    #[Route('/category/communication', name: 'app_category_communication')]
+    public function communication(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 2]);
+    
+        return $this->render('category/communication.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/entertainment', name: 'app_category_entertainment')]
+    public function entertainment(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 3]);
+    
+        return $this->render('category/entertainment.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/productivity', name: 'app_category_productivity')]
+    public function productivity(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 4]);
+    
+        return $this->render('category/productivity.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/social-media', name: 'app_category_social_media')]
+    public function social_media(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 5]);
+    
+        return $this->render('category/social-media.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/online-services', name: 'app_category_online_services')]
+    public function online_services(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 6]);
+    
+        return $this->render('category/online-services.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/dev', name: 'app_category_dev')]
+    public function dev(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 7]);
+    
+        return $this->render('category/dev.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/app', name: 'app_category_app')]
+    public function app(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 8]);
+    
+        return $this->render('category/app.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    #[Route('/category/games', name: 'app_category_games')]
+    public function games(ProductsRepository $productsRepository): Response
+    {
+        $products = $productsRepository->findBy(['category' => 9]);
+    
+        return $this->render('category/games.html.twig', [
+            'controller_name' => 'CategoryController',
+            'products' => $products,
+        ]);
+    }
+    
 }

e75d5c89abc82e66ba0a354b741a57e07d62725c
Max <max.pcgt@gmail.com>
2025-04-11 15:17:24 +0200

    🐛 fix: Production website bug fix - v.0.2.31

--- public/index.php
+++ public/index.php
@@ -3,12 +3,11 @@
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
-if (getenv('APP_ENV') === 'prod') {
-    $_SERVER['APP_ENV'] = 'prod';
-    $_SERVER['APP_DEBUG'] = '0';
-}
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+if (!isset($_SERVER['APP_ENV']) || ($_SERVER['APP_ENV'] !== 'prod' && file_exists(dirname(__DIR__).'/.env'))) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
